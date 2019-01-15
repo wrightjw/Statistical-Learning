52ff172470e186058b1aacd542ecbd5a8eefb3ee
James Wright <42690166+wrightjw@users.noreply.github.com>
2019-01-15 23:29:48 +0000

    performance assessment

--- estimation.r
+++ estimation.r
@@ -29,7 +29,22 @@ lines(density(x1),col=’red’)
 abline(v=5.42,lty=3,lwd=2)
 
 #performance assessment
-y <-numeric(length(x))
-y[x<=gamma] <- 0
-y[x>gamma] <- 1
-sum(y!=truey)/length(y)
+y <- c(rep(0,length(x0)), rep(1,length(x1)))
+n <- length(x)
+#randomly choose the training set. We do it by selecting indexes so that we assign paired vales of x and y to both sets. The set.seed
+#function initialises the ranodm number generator so that we get
+#the same random numbers every time we run the code (so the
+# results can be replicated)
+set.seed(1)
+trainindices <- sample(1:n, round(0.66*n), replace=FALSE)
+trainx <- x[trainindices]
+trainy <- y[trainindices]
+testx <- x[-trainindices]
+testy <- y[-trainindices]
+
+gamma <- findBoundary(trainx,trainy)
+ypreds <- numeric(length(testx))
+ypreds[testx<=gamma] <- 0
+ypreds[testx>gamma] <- 1
+
+sum(ypreds!=testy)/length(testy)
